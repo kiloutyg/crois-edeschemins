8c64869a90bf66c7605f8f68553b8916b8aae82d
kiloutyg <floriandkhissi@gmail.com>
2022-12-21 23:21:45 +0100

    on continu le voyage d'hoffman

---
+++ DirectoryTree.java
@@ -0,0 +1,3 @@
+public class DirectoryTree({"/home/josh/project/app/src/index.js","/home/peter/.bashrc","/home/josh/project/app/images/logo1.png","/home/josh/project/app/images/logo2.png","/home/peter/.profile","/home/peter/test","/var/log","/usr/lib/node14","/home/josh/project/app/test.jpg","/home/josh/project/app/images/logo3.png","/opt/apache2","etc/hosts"
+)
+}
--- Tree.java
+++ Tree.java
@@ -91,6 +91,6 @@ class Tree {
 }
 
 // Exemple d'utilisation de la solution
-Tree tree = new Tree();
-tree.buildTree(new String[] {"/home/josh/project/app/src/index.js","/home/peter/.bashrc","/home/josh/project/app/images/logo1.png","/home/josh/project/app/images/logo2.png","/home/peter/.profile","/home/peter/test","/var/log","/usr/lib/node14","/home/josh/project/app/test.jpg","/home/josh/project/app/images/logo3.png","/opt/apache2","etc/hosts"});
-tree.display();
+// Tree tree = new Tree();
+// tree.buildTree(new String[] {"/home/josh/project/app/src/index.js","/home/peter/.bashrc","/home/josh/project/app/images/logo1.png","/home/josh/project/app/images/logo2.png","/home/peter/.profile","/home/peter/test","/var/log","/usr/lib/node14","/home/josh/project/app/test.jpg","/home/josh/project/app/images/logo3.png","/opt/apache2","etc/hosts"});
+// tree.display();
